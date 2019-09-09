91175c63c8208f17b34b908127da83962748a4eb
Rémy Marronnier <rmarronnier@gmail.com>
2019-09-09 21:11:10 +0200

    Token ready

--- src/cadmium/lemmatizer/token.cr
+++ src/cadmium/lemmatizer/token.cr
@@ -8,10 +8,10 @@ module Cadmium
       # Check whether we're dealing with an uninflected paradigm, so we can
       # avoid lemmatization entirely.
       return false if self.morphology.nil?
-      # others = self.morphology.keys.map { |key| !["Number", "POS", "VerbForm", "Tense"].includes?(key) }
+      others = self.morphology.keys.map { |key| !["Number", "POS", "VerbForm", "Tense"].includes?(key) }
       return true if self.univ_pos == "noun" && self.morphology["number"] == "sing"
       return true if self.univ_pos == "verb" && self.morphology["verbform"] == "inf"
-      # return true if self.univ_pos == "verb" && (self.morphology["verbform"] == "fin" && self.morphology["tense"] == "pres" && self.morphology["number"].nil? && others.empty?) #cyclomatic complexity
+      return true if self.univ_pos == "verb" && (self.morphology["verbform"] == "fin" && self.morphology["tense"] == "pres" && self.morphology["number"].nil? && others.empty?) # cyclomatic complexity
       return true if self.univ_pos == "adj" && self.morphology["degree"] == "pos"
       return true if self.morphology.values.includes?(["VerbForm_inf", "VerbForm_none", "Number_sing", "Degree_pos"])
       false
